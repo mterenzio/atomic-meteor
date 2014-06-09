2542ecad5283fc1787cda3373ba9a32db7a21029
System Administrator <kingservice2014@gmail.com>
2014-06-09 16:10:38 +0800

    update

--- client/views/home/schemaupload.js
+++ client/views/home/schemaupload.js
@@ -10,8 +10,20 @@ Template.schemas.events({
       obj[data[0]] = data[1];
     }
     Meteor.call("insertCollectionData", Session.get("collectionName"), obj, function (error, result) {
-      
+
     });
 
+  }
+});
+
+Template.schemaList.events({
+  'click .removeCollection': function(events) {
+    var collection = CollectionData.findOne({_id: this._id}).collectionName;
+    CollectionData.remove({_id: this._id});
+    // MyCollections[collection]
+    // console.log(CollectionData.findOne({_id: this._id}).collectionName);
+    // var collections = CollectionData.findOne({_id: this._id});
+    // console.log(collections);
+    // console.log(this._id);
   }
 })
